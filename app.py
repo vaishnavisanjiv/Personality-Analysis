8b6de8ea83b75b05186593f8bc0c004187c45d65
vaishnavisanjiv <vaishnavisanjiv2312@gmail.com>
2025-01-26 11:43:40 +0530

    updated file

--- app.py
+++ app.py
@@ -78,4 +78,5 @@ def train_model():
     return render_template("train.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to port 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
